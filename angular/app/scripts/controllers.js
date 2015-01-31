b1eb927678d13314894376dd25ad98086b432060
Aurélien Matouillot <a.matouillot@gmail.com>
2015-01-31 21:31:04 +0100

    Since the app will be small, prefer to have only one controller file

--- angular/app/scripts/controllers.js
+++ angular/app/scripts/controllers.js
@@ -15,3 +15,20 @@ angular.module('App')
       'Karma'
     ];
   });
+
+
+/**
+ * @ngdoc function
+ * @name App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the App
+ */
+angular.module('App')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
